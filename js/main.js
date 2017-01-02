82ed7138461ca816d6cc7df3a9f6a54dff1d9ef0
Jason Carney <jason.jay.carney@gmail.com>
2017-01-02 15:00:34 +0930

    starting point

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+* Scripts for {Project name and url}
+*
+* author: {name}
+* email: {email}
+* website: {portfolio}
+*
+* Functions
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions
+*/
