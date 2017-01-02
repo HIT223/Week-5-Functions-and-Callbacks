068c8c8218dbea1c0795170116b21ba11f9ef0c3
Jason Carney <jason.jay.carney@gmail.com>
2017-01-02 15:06:57 +0930

    add basic functions

--- js/main.js
+++ js/main.js
@@ -8,3 +8,38 @@
 * Functions
 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions
 */
+
+/*
+* TASK 1: Function declaration
+*/
+
+
+makeHeadingRed();
+
+makeYellow('#pageTitle');
+
+makeColor('#pageTitle', 'blue');
+
+/*
+* basic function, no args
+*/
+function makeHeadingRed(){
+	var heading = document.querySelector('#pageTitle');
+	heading.style.color = 'red';
+}
+
+/*
+* function with args
+*/
+function makeYellow(selector){
+	var node = document.querySelector(selector);
+	node.style.color = 'yellow';
+}
+
+/*
+* function with mutiple args
+*/
+function makeColor(selector, color){
+	var node = document.querySelector(selector);
+	node.style.color = color;
+}
