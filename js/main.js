8dee188fd2e249691195255d26c717d7f64a758a
Jason Carney <jason.jay.carney@gmail.com>
2017-01-02 16:13:16 +0930

    complete

--- js/main.js
+++ js/main.js
@@ -43,3 +43,60 @@ function makeColor(selector, color){
 	var node = document.querySelector(selector);
 	node.style.color = color;
 }
+
+
+/*
+* TASK 2: Function expressions 
+*/
+
+var sendAlert = function(message){
+	alert(message);
+}
+
+changeColor('#pageTitle', 'black', sendAlert);
+
+
+changeColor('#pageTitle', 'green', function(message){
+	alert(message);
+});
+
+function changeColor(selector, color, callback){
+	var node = document.querySelector(selector);
+	node.style.color = color;
+
+	callback(selector + ' Changed to: '+ color);
+}
+
+/*
+* TASK 3: Immediately Invokable Function Expression 
+*/
+
+(function(){
+	alert('warning: This is a Immediately Invokable Function Expression');
+})();
+
+(function(prefix){
+	alert(prefix + ': This is a Immediately Invokable Function Expression');
+})('warning');
+
+/*
+* TASK 4: JSON parameters
+*/
+
+var options = {
+	selector: "#pageTitle",
+	color: "red",
+	callback: function(message){
+		alert(message)
+	}
+};
+
+changeColorJSON(options);
+
+function changeColorJSON(options){
+	var node = document.querySelector(options.selector);
+	node.style.color = options.color;
+
+	options.callback(options.selector + ' Changed to: '+ options.color);
+}
+
