537bcbe530b71638f2bc6880e9f2e7e127845735
Jason Carney <jason.jay.carney@gmail.com>
2017-03-29 22:03:52 +0930

    add comments

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@
 * Functions
 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions
 */
+
+
+/*
+* TASK 1: Function declaration
+*/
+
+
+
+/*
+* TASK 2: Function expressions
+*/
+
+
+
+/*
+* TASK 3: Immediately Invokable Function Expression
+*/
+
+
+
+/*
+* TASK 4: JSON parameters
+*/
